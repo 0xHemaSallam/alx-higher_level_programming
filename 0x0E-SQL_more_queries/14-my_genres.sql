268c0aef47b2bea6ad9441e9193dfbf0dfd21983
0xmuka <hemasallam03@gmail.com>
2024-03-15 21:33:05 +0200

    task-sql

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -1,8 +1,4 @@
--- Use 'hbtn_0d_tvshows' to list all genres of show 'Dexter'
--- 'tv_shows' table contains only one record where title = Dexter
--- Each record should display tv_genres.name
--- Results must be sorted in ascending order by genre name
--- You can only use one SELECT statement
+--14
 SELECT tv_genres.name
 FROM tv_genres
 INNER JOIN tv_show_genres m ON tv_genres.id = m.genre_id
--- 0x0E-SQL_more_queries/15-comedy_only.sql
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -1,7 +1,4 @@
--- List all Comedy shows in 'hbtn_0d_tvshows'
--- 'tv_genres' table contains only one record where name = Comedy
--- Results must be sorted in ascending order by the show title
--- You can only use one SELECT statement
+-- 15
 SELECT tv_shows.title
 FROM tv_shows
 INNER JOIN tv_show_genres m ON tv_shows.id = m.show_id
