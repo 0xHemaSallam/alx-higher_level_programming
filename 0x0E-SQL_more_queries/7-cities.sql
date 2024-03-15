8f9bdd0e0e84688fcfb080f2e9181419e8c0d0b6
0xmuka <hemasallam03@gmail.com>
2024-03-15 20:47:37 +0200

    task-sql

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -1,7 +1,8 @@
 -- 7-cities.sql
 
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
-
 CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities (
-    id INT UNIQUE AUTO_INCREMENT NOT NULL PRIMARY KEY, state_id INT NOT NULL FOREIGN KEY REFERENCES hbtn_0d_usa.states(id), name VARCHAR(256) NOT NULL
-);
+       id INT UNIQUE NOT NULL AUTO_INCREMENT PRIMARY KEY,
+       state_id INT NOT NULL,
+       FOREIGN KEY(state_id) REFERENCES hbtn_0d_usa.states(id),
+       name VARCHAR(256) NOT NULL);
