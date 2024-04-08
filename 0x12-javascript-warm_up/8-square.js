23fb4c5637c578d83866ab2d6e2d0d6ffea9fa9c
0xmuka <hemasallam03@gmail.com>
2024-04-08 22:51:15 +0200

    task8

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,12 +1,11 @@
 #!/usr/bin/node
 
 const firstArg = process.argv[2];
-const parsedInt = parseInt(firstArg);
 
-if (isNaN(parsedInt)) {
+if (isNaN(firstArg)) {
   console.log('Missing size');
 } else {
-  for (let i = 0; i < parsedInt; i++) {
-    console.log('#'.repeat(parsedInt));
+  for (let i = 0; i < firstArg; i++) {
+    console.log('#'.repeat(firstArg));
   }
 }
