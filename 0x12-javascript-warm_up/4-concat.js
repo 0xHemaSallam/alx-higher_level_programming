51813afe8c9580ffed404dd8ac6b5194b6ca929f
0xmuka <hemasallam03@gmail.com>
2024-04-08 15:11:45 +0200

    task5

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -1,4 +1,4 @@
 #!/usr/bin/node
-const process = require('process'); // Import the process object
+const process = require('process');
 
 console.log(`${process.argv[2]} is ${process.argv[3] || 'undefined'}`);
---
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+const process = require('process');
+
+const firstArg = process.argv[2];
+const parsedInt = parseInt(firstArg);
+
+console.log(isNaN(parsedInt) ? "Not a number" : `My number: ${parsedInt}`);
