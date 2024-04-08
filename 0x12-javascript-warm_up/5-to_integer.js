502b5d10b36755705bcdaf0e4231f7af4e9df4bf
0xmuka <hemasallam03@gmail.com>
2024-04-08 15:12:18 +0200

    task5

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -4,4 +4,4 @@ const process = require('process');
 const firstArg = process.argv[2];
 const parsedInt = parseInt(firstArg);
 
-console.log(isNaN(parsedInt) ? "Not a number" : `My number: ${parsedInt}`);
+console.log(isNaN(parsedInt) ? 'Not a number' : `My number: ${parsedInt}`);
